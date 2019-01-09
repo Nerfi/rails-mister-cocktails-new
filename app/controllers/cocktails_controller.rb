52976e397d9ac79623cb75cdc669c76fa67ae314
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-09 18:23:12 +0100

    .

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,8 +4,27 @@ class CocktailsController < ApplicationController
   end
 
 
+  def new
+    @cocktail = Cocktail.new
+
+  end
+
   def show
       @cocktail = Cocktail.find(params[:id])
+
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+  end
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
 
   end
 end
